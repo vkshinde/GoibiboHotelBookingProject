78b20d54ceb6344cc02f5a6ca93b999fa8bc62b9
Vishal Shinde <67584912+vkshinde@users.noreply.github.com>
2023-02-04 11:54:03 +0530

    Update App.java

--- src/main/java/ProjectObjectModal/POM/App.java
+++ src/main/java/ProjectObjectModal/POM/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World! Please check the Test Folder--- YOu are in developemnt Folder" );
     }
 }
